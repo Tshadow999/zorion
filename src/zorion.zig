660037973637f5accb5dd8c2907f9d727f6707cf
Tshadow999 <tychoschurink@outlook.com>
2024-10-15 11:44:54 +0200

    First triangle (without a shader)

--- src/zorion.zig
+++ src/zorion.zig
@@ -34,11 +34,36 @@ pub fn main() !void {
     const proc: glfw.GLProc = undefined;
     try gl.load(proc, glGetProcAddress);
 
+    // Contruct triangle position
+    const verts = [9]f32{
+        -0.5, -0.5, 0.0,
+        0.5,  -0.5, 0.0,
+        0.0,  0.5,  0.0,
+    };
+
+    // Contruct Vertex array
+    var vertexArray: u32 = undefined;
+    gl.genVertexArrays(1, &vertexArray);
+    gl.bindVertexArray(vertexArray);
+
+    // Contruct Vertex buffer
+    var vertexBuffer: u32 = undefined;
+    gl.genBuffers(1, &vertexBuffer);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, verts.len * @sizeOf(f32), &verts, gl.STATIC_DRAW);
+
+    gl.vertexAttribPointer(0, 3, gl.FLOAT, gl.FALSE, 3 * @sizeOf(f32), null);
+    gl.enableVertexAttribArray(0);
+
     while (!window.shouldClose()) {
         glfw.pollEvents();
 
         gl.clearColor(0.3, 0.1, 0.3, 1);
         gl.clear(gl.COLOR_BUFFER_BIT);
+
+        // Draw triangle
+        gl.drawArrays(gl.TRIANGLES, 0, 3);
 
         window.swapBuffers();
     }
