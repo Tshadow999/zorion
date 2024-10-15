51b986aba3e5761f1ac79b87b2f9f70823b805a3
Tshadow999 <tychoschurink@outlook.com>
2024-10-15 10:54:45 +0200

    Init zorion engine with working window

---
+++ build.zig
@@ -0,0 +1,82 @@
+const std = @import("std");
+const mach_glfw = @import("mach_glfw");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "glfw-test",
+        .root_source_file = b.path("src/zorion.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    addDependencies(exe, b, target, optimize);
+
+    b.installArtifact(exe);
+
+    // Not sure what this is
+    // const exe_check = b.addExecutable(.{
+    //     .name = "glfw-test",
+    //     .root_source_file = b.path("src/zorion.zig"),
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
+
+    // addDependencies(exe_check, b, target, optimize);
+
+    // const check = b.step("check", "Check if it compiles");
+    // check.dependOn(&exe_check.step);
+
+    const run_cmd = b.addRunArtifact(exe);
+
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+}
+
+// This is where all the dependencies should be added
+// Copied from Shakedown Engine
+fn addDependencies(
+    exe: *std.Build.Step.Compile,
+    b: *std.Build,
+    target: std.Build.ResolvedTarget,
+    optimize: std.builtin.OptimizeMode,
+) void {
+    // Use mach-glfw
+    const glfw_dep = b.dependency("mach_glfw", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    exe.root_module.addImport("mach-glfw", glfw_dep.module("mach-glfw"));
+
+    // gl
+    exe.root_module.addImport("gl", b.createModule(.{
+        .root_source_file = b.path("deps/gl41.zig"),
+    }));
+
+    // Maybe need this later
+    // zgltf
+    // exe.root_module.addImport("zgltf", b.dependency("zgltf", .{
+    //     .target = target,
+    //     .optimize = optimize,
+    // }).module("zgltf"));
+
+    // mach math
+    // exe.root_module.addImport("math", b.createModule(.{
+    //     .root_source_file = b.path("deps/math/main.zig"),
+    // }));
+
+    // Include C
+    exe.linkLibC();
+    // exe.addCSourceFile(.{ .file = b.path("deps/stb_image.c"), .flags = &.{} });
+    // exe.addCSourceFile(.{ .file = b.path("deps/cgltf.c"), .flags = &.{} });
+
+    exe.addIncludePath(b.path("deps"));
+}
---
+++ src/zorion.zig
@@ -0,0 +1,45 @@
+const std = @import("std");
+const glfw = @import("mach-glfw");
+const gl = @import("gl");
+
+fn glGetProcAddress(p: glfw.GLProc, proc: [:0]const u8) ?gl.FunctionPointer {
+    _ = p;
+    return glfw.getProcAddress(proc);
+}
+
+fn errorCallback(error_code: glfw.ErrorCode, description: [:0]const u8) void {
+    std.log.err("glfw:{}: {s}\n", .{ error_code, description });
+}
+
+pub fn main() !void {
+    glfw.setErrorCallback(errorCallback);
+    if (!glfw.init(.{})) {
+        std.log.err("Failed to init glfw: {?s}", .{glfw.getErrorString()});
+        std.process.exit(1);
+    }
+    defer glfw.terminate();
+
+    const window = glfw.Window.create(
+        1280,
+        720,
+        "Zorion Engine",
+        null,
+        null,
+        .{},
+    ).?;
+    defer window.destroy();
+
+    glfw.makeContextCurrent(window);
+
+    const proc: glfw.GLProc = undefined;
+    try gl.load(proc, glGetProcAddress);
+
+    while (!window.shouldClose()) {
+        glfw.pollEvents();
+
+        gl.clearColor(0.3, 0.1, 0.3, 1);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+
+        window.swapBuffers();
+    }
+}
